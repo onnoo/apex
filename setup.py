d5ae3a1ec0bce97810d9cdca167b7b11fb1b9e8d
Jaewoo Yang <sweyjw@gmail.com>
2019-12-04 15:34:35 +0900

    Fix misspell
    line 74 string concatenation
    
    add + operation.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def check_cuda_torch_binary_vs_bare_metal(cuda_dir):
                            "not match the version used to compile Pytorch binaries.  " +
                            "Pytorch binaries were compiled with Cuda {}.\n".format(torch.version.cuda) +
                            "In some cases, a minor-version mismatch will not cause later errors:  " +
-                           "https://github.com/NVIDIA/apex/pull/323#discussion_r287021798.  "
+                           "https://github.com/NVIDIA/apex/pull/323#discussion_r287021798.  " +
                            "You can try commenting out this check (at your own risk).")
 
 # Set up macros for forward/backward compatibility hack around
